d973544a1b9f5a31c6d1ad7b07189cd26558c1d4
AyanleHashi <ahashi2000@gmail.com>
2016-07-12 12:34:58 -0400

    Update XML Printer V2.py

--- XML Printer V2.py
+++ XML Printer V2.py
@@ -9,33 +9,38 @@ outputFile = open('output.csv', 'wb')
 with open("H:\Pubs_basedon_TCIA.xml") as fd:
     doc = xmltodict.parse(fd.read())
 
-for i in range(0,len(doc["xml"]["records"]["record"])):
-    try:
-        for author in range(0,len(doc["xml"]["records"]["record"][i]["authors"])):
-            doc_authors[0].append(doc["xml"]["records"]["record"][i]["contributors"]["authors"]["author"][author][u'style']["#text"])
-    except KeyError:
-        pass
-
-doc_title.append(doc["xml"]["records"]["record"][0]["titles"]["title"][u'style']["#text"])
-doc_periodical.append(doc["xml"]["records"]["record"][0]["periodical"]["full-title"][u'style']["#text"])
-doc_year.append(doc["xml"]["records"]["record"][0]["dates"]["year"][u'style']["#text"])
+for i in range(0,len(doc['xml']['records']['record'])):
+    if doc["xml"]["records"]["record"][i]["ref-type"]["@name"] == "Journal Article":
+        print ''
+        for author in range(0,len(doc['xml']['records']['record'][i]['contributors']['authors']['author'])):
+            try:
+                print doc['xml']['records']['record'][i]['contributors']['authors']['author'][author][u'style']['#text']
+            except KeyError:
+                print doc['xml']['records']['record'][i]['contributors']['authors']['author'][u'style']['#text']
 
 for i in range(0,len(doc["xml"]["records"]["record"])):
     if doc["xml"]["records"]["record"][i]["ref-type"]["@name"] == "Journal Article":
         try:
             doctitletemp = doc["xml"]["records"]["record"][i]["titles"]["title"][u'style']["#text"]
             doc_title.append(doctitletemp)
-            'print doctitletemp'
+            print doctitletemp
             
             docperiodicaltemp = doc["xml"]["records"]["record"][i]["periodical"]["full-title"][u'style']["#text"]
             doc_periodical.append(docperiodicaltemp)
-            'print docperiodicaltemp'
+            print docperiodicaltemp
             
             docyeartemp = doc["xml"]["records"]["record"][i]["dates"]["year"][u'style']["#text"]
             doc_year.append(docyeartemp)
-            'print docyeartemp'
+            print docyeartemp
         except TypeError:
-            pass
+            try:
+                doctitletemp = doc["xml"]["records"]["record"][i]["titles"]["title"][u'style'][0]["#text"]
+                doc_title.append(doctitletemp)
+                print doctitletemp
+            except KeyError:
+                doctitletemp = doc["xml"]["records"]["record"][i]["titles"]["title"][u'style'][0][0]["#text"]
+                doc_title.append(doctitletemp)
+                print doctitletemp
         except KeyError:
             docperiodicaltemp = doc["xml"]["records"]["record"][1]["alt-periodical"]["full-title"][u'style']["#text"]
             doc_periodical.append(docperiodicaltemp)
