614591852bcb3ff56ab6372f76fcb1642a6421ff
AyanleHashi <ahashi2000@gmail.com>
2016-07-14 12:34:30 -0400

    Update XML Printer V2.py

--- XML Printer V2.py
+++ XML Printer V2.py
@@ -1,4 +1,4 @@
-#Final version of the code
+#Updated to work with all versions of Pubs_basedon_TCIA----
 import xmltodict
 import csv
 
@@ -27,33 +27,26 @@ for i in range(0,len(doc["xml"]["records"]["record"])):
         try:
             docyeartemp = doc["xml"]["records"]["record"][i]["dates"]["year"][u'style']["#text"]
             doc_year.append(docyeartemp)
-            print docyeartemp
             
             doctitletemp = doc["xml"]["records"]["record"][i]["titles"]["title"][u'style']["#text"]
             doc_title.append(doctitletemp)
-            print doctitletemp
             
             docperiodicaltemp = doc["xml"]["records"]["record"][i]["periodical"]["full-title"][u'style']["#text"]
             doc_periodical.append(docperiodicaltemp)
-            print docperiodicaltemp
         except KeyError:
             try:
                 docperiodicaltemp = doc["xml"]["records"]["record"][i]["alt-periodical"]["full-title"][u'style']["#text"]
                 doc_periodical.append(docperiodicaltemp)
-                print docperiodicaltemp
             except KeyError:
                 docperiodicaltemp = 'None'
                 doc_periodical.append(docperiodicaltemp)
-                print docperiodicaltemp
         except TypeError:
             try:
                 doctitletemp = doc["xml"]["records"]["record"][i]["titles"]["title"][u'style'][0]["#text"]
                 doc_title.append(doctitletemp)
-                print doctitletemp
             except KeyError:
                 doctitletemp = doc["xml"]["records"]["record"][i]["titles"]["title"][u'style'][0][0]["#text"]
                 doc_title.append(doctitletemp)
-                print doctitletemp
 
 doc_authors = [[y.encode('UTF8') for y in x] for x in doc_authors]
 doc_title = [x.encode('UTF8') for x in doc_title]
@@ -62,14 +55,10 @@ doc_periodical = [x.encode('UTF8') for x in doc_periodical]
 doc_periodical.insert(39,'Magnetic Resonance Imaging')
 
 outputWriter = csv.writer(outputFile)
-outputWriter.writerow(['Authors:'])
-outputWriter.writerow(doc_authors)
-outputWriter.writerow(['Title:'])
-outputWriter.writerow(doc_title)
-outputWriter.writerow(['Periodical:'])
-outputWriter.writerow(doc_periodical)
-outputWriter.writerow(['Year:'])
-outputWriter.writerow(doc_year)
-outputWriter.writerow('')
-outputWriter.writerow(['Each column is a separate record.'])
+outputWriter.writerow(['Authors:', 'Titles:', 'Periodicals:', 'Years:'])
+try:
+    for record in range(0,len(doc['xml']['records']['record'])):
+        outputWriter.writerow([doc_authors[record], doc_title[record], doc_periodical[record], doc_year[record]])
+except IndexError:
+    print 'Done writing to file.'
 outputFile.close()
