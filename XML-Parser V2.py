94c2028520ddd4132c2c965b9d1a8b136842e551
Ayanle Hashi <ahashi2000@gmail.com>
2018-06-05 15:41:04 -0400

    Create XML-Parser V2.py

---
+++ XML-Parser V2.py
@@ -0,0 +1,85 @@
+from bs4 import BeautifulSoup
+
+class Record:
+    def __init__(self,authors="",title="",periodical="",year="",pubtype=""):
+        self.authors = authors
+        self.title = title
+        self.periodical = periodical
+        self.year = year
+        self.pubtype = pubtype
+    
+    def __str__(self):
+        return "Authors: " + self.authors + "\nTitle: " + self.title +\
+        "\nPeriodical: " + self.periodical + "\nYear: " + self.year +\
+        "\nPublication Type: " + self.pubtype
+
+with open("C:\\Users\\hashiam\\Desktop\\Python Scripts\\Pubs_basedon_TCIA0518.xml",encoding="utf8") as f:
+    xml = f.read()
+
+records = []
+soup = BeautifulSoup(xml,"xml")
+
+for record in soup.xml.records:
+    authors = ""
+    for author in record.contributors.authors:
+        authors += author.text + "; "
+    authors = authors[:-2]
+    title = record.titles.title.text
+    try:
+        periodical = record.periodical.find_all("full-title")[0].text
+    except AttributeError:
+        periodical = "None"
+    year = record.dates.year.text
+    pubtype = record.find_all("ref-type")[0]["name"]
+    records.append(Record(authors,title,periodical,year,pubtype))
+
+row = ""
+for r in records[:1]:
+        row += """    <tr>
+        <td>{}</td>
+        <td>{}</td>
+        <td>{}</td>
+        <td>{}</td>
+        <td>{}</td>
+    </tr>
+""".format(r.authors,r.title,r.periodical,r.year,r.pubtype)
+
+print("""<html>
+  <table style=\"width:100%\">
+    <tr>
+      <th>Authors</th>
+      <th>Title</th>
+      <th>Periodical</th>
+      <th>Year</th>
+      <th>Publication Type</th>
+    </tr>
+{}
+  </table>
+</html>
+
+table, th, td {{
+    border: 1px solid black;
+}}
+th {{
+    text-align: left;
+}}""".format(row))
+
+print("""
+<html>
+<h3>{}</h3>
+  {}
+  <br>
+  <periodical>{}</periodical>, {}
+  <pub-type> - {}</pub-type>
+</html>
+
+html {{
+  font-family: "Arial";
+}}
+periodical {{
+  color: green;
+}}
+
+pub-type {{
+  color: LightGray;
+}}""".format(records[0].title,records[0].authors,records[0].periodical,records[0].year,records[0].pubtype))
