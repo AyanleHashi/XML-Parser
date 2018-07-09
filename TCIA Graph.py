5df8c660f049e4cba4de89f9e605f28001646896
Ayanle Hashi <ahashi2000@gmail.com>
2018-07-09 13:11:54 -0400

    Update TCIA Graph.py

--- TCIA Graph.py
+++ TCIA Graph.py
@@ -16,10 +16,12 @@ for record in soup.xml.records:
     else:
         d[year] += 1
 
+"This pulls the years from the actual publication XML"
 years = sorted([int(x) for x in d.keys()])
 count = sorted(d.values())
 total = list(np.cumsum(count))
 
+"Preset data taken from the TCIA Publications page"
 #years = [2010,2011,2012,2013,2014,2015,2016,2017,2018]
 #count = [11,  15,  21,  47,  76,  116, 140, 134, 67]
 #total = [11,  26,  47,  94,  170, 286, 426, 560, 627]
