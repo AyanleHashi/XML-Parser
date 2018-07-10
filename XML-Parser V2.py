0c38db440e10e55a45160218ec6829c38fbb90cf
Ayanle Hashi <ahashi2000@gmail.com>
2018-07-10 15:49:10 -0400

    Update XML-Parser V2.py

--- XML-Parser V2.py
+++ XML-Parser V2.py
@@ -5,9 +5,13 @@ import subprocess
 The main problem is that Google Scholar does not allow too many requests at a 
 time, and given how large the publication XML file is, it blocks you from 
 getting all of the URLs in one go.
+
+Potential Solutions:
+    -Have the script run in multiple parts
+    -Add time.sleep(10) after each element in the for loop (change time if needed)
 """
 
-path = "C:\\Users\\hashiam\\Desktop\\Python Scripts\\Pubs_basedon_TCIA0618.xml"
+path = "/home/ayanlehashi/mysite/scripts/Pubs_basedon_TCIA0618.xml"
 
 class Record:
     def __init__(self,authors="",title="",periodical="",year="",pubtype="",url="",abstract=""):
@@ -21,15 +25,15 @@ class Record:
     
     def __str__(self):
         return """
-  <div>
-  <h4>%s</h4>
-  %s
-  <br>
-  <i><periodical>%s</periodical></i> %s
-  <pub-type> - %s</pub-type>
-  <br>
-  %s%s
-  </div>
+    <div id="paper">
+        <h4>%s</h4>
+        %s
+        <br>
+        <periodical>%s</periodical> %s
+        <pub-type> %s</pub-type>
+        <br>
+        %s%s
+    </div>
  """ % self.tuple_form()
     
     def tuple_form(self):
@@ -40,6 +44,7 @@ with open(path,encoding="utf8") as f:
 
 records = []
 soup = BeautifulSoup(xml,"lxml")
+abstract_number = 1
 for record in soup.xml.records:
     authors = ""
     for author in record.contributors.authors:
@@ -53,9 +58,10 @@ for record in soup.xml.records:
     year = record.dates.year.text
     pubtype = record.find_all("ref-type")[0]["name"]
     try:
-        url = "<a href=\"" + record.urls.find_all("related-urls")[0].url.text + "\">Website</a> - "
+        url = "<a href=\"" + record.urls.find_all("related-urls")[0].url.text + "\">Website</a>"
     except IndexError:
         url = ""
+        """
         #number of citations (scholar link too)
         #number of total citations per year (add to graph?)
         command = "scholar.py -c 1 --phrase \"{}\"".format(title)
@@ -63,15 +69,17 @@ for record in soup.xml.records:
         out = process.communicate()[0].decode("cp1252").replace(r"\r","")
         url = out[out.find("URL")+4:out.find("Year")][:-12]
         
-        if "scholar" in url:
+        if "scholar.google.com" in url:
             url = url[26:]
-        url = "<a href=\"" + url + "\">Website</a> - "
-        
+        if len(url) != 0:
+            url = "<a href=\"" + url + "\">Website</a>"
+        """
     try:
-        abstract = record.abstract.text
+        abstract = """<button type="button" class="btn btn-link" data-toggle="collapse" data-target="#demo{0}"><span class="glyphicon glyphicon-arrow-down"></span>Abstract</button>
+        <div id="demo{0}" class="collapse">{1}</div>""".format(abstract_number,record.abstract.text)
+        abstract_number += 1
     except AttributeError:
         abstract = ""
-        url = url[:-3]
     if pubtype in ["Journal Article","Conference Proceedings"]:
         records.append(Record(authors,title,periodical,year,pubtype,url,abstract))
 
@@ -80,74 +88,40 @@ for r in records:
     entry += str(r)
 
 paperpile_html = """<html>
-  <style>
-    html {{
-      font-family: "Helvetica";
-      line-height:150%
-    }}
-    periodical {{
-      color: green;
-    }}
-    pub-type {{
-      color: #666666;
-    }}
-    h4 {{
-      margin-bottom: 0px;
-      color: #064361;
-    }}
-    a {{
-      text-decoration: none;
-    }}
-    div {{
-      margin-right: 25em;
-      margin-left: 35em;
-    }}
-  </style>  
-  {}
+    <head>
+        <style>
+            html {{
+                font-family: "Helvetica";
+                line-height:150%
+            }}
+            periodical {{
+                color: green;
+                font-style: italic;
+            }}
+            pub-type {{
+                color: #666666;
+            }}
+            h4 {{
+                margin-bottom: 0px;
+                color: #0fa5f0;
+            }}
+            a {{
+                text-decoration: none;
+            }}
+            #paper {{
+                border: 1px solid #DDDDDD
+            }}
+        </style>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <div class="container">
+        {}
+    </div>
 </html>
 """.format(entry)
 
-with open("paperpile.html","w",encoding="utf8") as paperpile_html_file:
+with open("/home/ayanlehashi/mysite/templates/paperpile.html","w",encoding="utf8") as paperpile_html_file:
     paperpile_html_file.write(paperpile_html)
-
-#------------------------------------------------------------------------------
-
-row = ""
-for r in records:
-    row += """    <tr>
-        <td>%s</td>
-        <td>%s</td>
-        <td>%s</td>
-        <td>%s</td>
-        <td>%s</td>
-        <td>%s</td>
-        <td>%s</td>
-    </tr>
-""" % r.tuple_form()
-
-table_html = """<html>
-  <style>
-      table, th, td {{
-        border: 1px solid black;
-      }}
-      th {{
-        text-align: left;
-      }}
-  </style>
-  
-  <table style=\"width:100%\">
-    <tr>
-      <th>Title</th>
-      <th>Authors</th>
-      <th>Periodical</th>
-      <th>Year</th>
-      <th>Publication Type</th>
-      <th>URL</th>
-      <th>Abstract</th>
-    </tr>
-{}
-  </table>
-</html>""".format(row)
-
-with open("table.html","w",encoding="utf8") as table_html_file:
-    table_html_file.write(table_html)
