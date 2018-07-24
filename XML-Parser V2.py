364ce1c9f5ceebbba6f4bc5e10f3e294017cfa87
Ayanle Hashi <ahashi2000@gmail.com>
2018-07-24 15:41:40 -0400

    Update XML-Parser V2.py

--- XML-Parser V2.py
+++ XML-Parser V2.py
@@ -16,7 +16,8 @@ Potential Solutions:
 path = "/home/ayanlehashi/mysite/scripts/Pubs_basedon_TCIA0618.xml"
 
 class Record:
-    def __init__(self,authors="",title="",periodical="",year="",pubtype="",url="",abstract="",citations=""):
+    def __init__(self,i="",authors="",title="",periodical="",year="",pubtype="",url="",abstract="",citations=""):
+        self.i = i
         self.authors = authors
         self.title = title
         self.periodical = periodical
@@ -26,30 +27,31 @@ class Record:
         self.abstract = abstract
         self.citations = citations
 
-        self.labels = []
-
     def __str__(self):
-        return """
-    <div class="paper droppable">
-        <h4>%s</h4>
-        %s
-        <br>
-        <periodical>%s</periodical> %s
-        <pub-type> - %s</pub-type> %s
-        <br>
-        %s%s
-    </div>
- """ % self.tuple_form()
+     return """
+        <div id="{0}" class="paper droppable">
+            <h4>{1}</h4>
+            {2}
+            <br>
+            <periodical>{3}</periodical> {4}
+            <pub-type> - {5}</pub-type> {6}
+            <br>
+            {7}{8}
+        </div><br>
+     """.format(*self.tuple_form())
 
     def tuple_form(self):
-        return (self.title,self.authors,self.periodical,self.year,self.pubtype,self.citations,self.url,self.abstract)
+        return (self.i,self.title,self.authors,self.periodical,self.year,self.pubtype,self.citations,self.url,self.abstract)
 
 with open(path,encoding="utf8") as f:
     xml = f.read()
 
 records = []
 soup = BeautifulSoup(xml,"lxml")
+
 abstract_number = 1
+record_number = 1
+
 for record in soup.xml.records:
     authors = ""
     for author in record.contributors.authors:
@@ -77,7 +79,7 @@ for record in soup.xml.records:
     except ValueError:
         citations = ""
     try:
-        url = "<a href=\"" + record.urls.find_all("related-urls")[0].url.text + "\">Website</a> - "
+        url = "<a href=\"" + record.urls.find_all("related-urls")[0].url.text + "\"><span class=\"glyphicon glyphicon-link\"></span>Website</a>"
     except IndexError:
         url = ""
         #url = out[out.find("URL")+4:out.find("Year")][:-12]
@@ -85,16 +87,16 @@ for record in soup.xml.records:
         if "scholar.google.com" in url:
             url = url[26:]
         if len(url) != 0:
-            url = "<a href=\"" + url + "\">Website</a> - "
+            url = "<a href=\"" + url + "\">Website</a>"
     try:
         abstract = """<button type="button" class="btn btn-link" data-toggle="collapse" data-target="#demo{0}"><span class="glyphicon glyphicon-arrow-down"></span>Abstract</button>
         <div id="demo{0}" class="collapse">{1}</div>""".format(abstract_number,record.abstract.text)
         abstract_number += 1
     except AttributeError:
         abstract = ""
-        url = url[:-3]
     #if pubtype in ["Journal Article","Conference Proceedings"]:
-    records.append(Record(authors,title,periodical,year,pubtype,url,abstract,citations))
+    records.append(Record(str(record_number),authors,title,periodical,year,pubtype,url,abstract,citations))
+    record_number += 1
 
 entry = ""
 for r in records:
@@ -106,35 +108,11 @@ paperpile_html = """<html>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+    <script src="/static/draggable.js"></script>
+
     <head>
-        <script type="text/javascript">
-            $(document).ready(function() {{
-                $(".draggable").draggable({{
-                    revert: true,
-                    revertDuration: 500
-                }});
-
-                $(".droppable").droppable({{
-                    activeClass: "active",
-                    drop: function (event, ui) {{
-                        if ($(this).text().indexOf(ui.draggable.text()) == -1) {{
-                            $(this).text($(this).text() + " " + ui.draggable.text());
-                        }}
-
-                        $(this).removeClass(ui.draggable.text());
-                        $(this).addClass(ui.draggable.text());
-                    }}
-                  }});
-
-                $("#searchbar").on("keyup", function() {{
-                    var value = $(this).val().toLowerCase();
-                    $(".container .paper").filter(function() {{
-                        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-                    }});
-                }});
-            }});
-        </script>
-
+        <title>TCIA Publications</title>
         <style>
             html {{
                 font-family: "Arial";
@@ -155,11 +133,15 @@ paperpile_html = """<html>
                 text-decoration: none;
             }}
             .paper {{
-                border: 1px solid #DDDDDD
+                /*border: 1px solid #DDDDDD;
+                border-radius: 5px;*/
+            }}
+            .container {{
+                padding-top: 20px;
             }}
             #sidebar {{
                 height: 500px;
-                width: 160px;
+                width: 100px;
                 position: fixed;
                 z-index: 1;
                 top: 0;
@@ -167,23 +149,28 @@ paperpile_html = """<html>
                 padding-top: 20px;
             }}
             .draggable {{
-                /*width: 10px;*/
                 height: 40px;
                 display: inline-block;
                 background: #ccc;
                 padding: 10px;
-                border-radius: 10px;
+                border-radius: 5px;
                 border: 1px solid #666;
             }}
-            .droppable {{
-                /*margin-left: 200px;
-                margin-bottom: 5px;
-                width: 125px;
-                height: 125px;
-                background: #999;
-                color: #fff;
+            .draggable-after {{
+                height: 40px;
+                display: inline-block;
+                background: #ccc;
+                border-radius: 5px;
+                border: 1px solid #666;
+                /*font-size: 12px;*/
+                /*padding: 5px;*/
                 padding: 10px;
-                border: 4px solid #999;*/
+            }}
+            .searchbar {{
+
+            }}
+            .btn-link {{
+                padding: 0px;
             }}
             {{% for label in labels %}}
             .{{{{ label }}}} {{}}
@@ -204,7 +191,10 @@ paperpile_html = """<html>
     </div>
 
     <div class="container">
-        <input id="searchbar" type="text" name="search" placeholder="Search...">
+        <div class="input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>
+            <input id="searchbar" type="text" class="form-control searchbar" name="Searchbar" placeholder="Search...">
+        </div>
         {}
     </div>
 </html>
